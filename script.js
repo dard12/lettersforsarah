1398d89aca6b04d3c5eeca0202e6fedaf43aa3a7
Ricky Lung <dard12@gmail.com>
2021-11-17 18:51:45 -0500

    nobody better read this unless you're sarah

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ function clickAffirmation() {
   const numberOfAffirmations = 3;
   const random = Math.floor(Math.random() * numberOfAffirmations) + 1;
 
-  window.location.href = "/affirmation-" + random + ".html";
+  // window.location.href = "/affirmation-" + random + ".html";
+  alert(
+    "This is coming soon. I don't know how soon, but I do know that I love you."
+  );
 }
