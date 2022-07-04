c539943c4edb19f174d6028634990217b501e75d
Ricky Lung <dard12@gmail.com>
2022-07-03 21:30:13 -0400

    airplanes and all that

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 function clickAffirmation() {
-  const numberOfAffirmations = 3;
+  const numberOfAffirmations = 1;
   const random = Math.floor(Math.random() * numberOfAffirmations) + 1;
 
-  // window.location.href = "/affirmation-" + random + ".html";
-  alert(
-    "This is coming soon. I don't know how soon, but I do know that I love you."
-  );
+  window.location.href = "./affirmation-" + random + ".html";
 }
