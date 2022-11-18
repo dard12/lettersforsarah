886fdef203437a1adb753d42143f6d8063b8489c
Ricky Lung <dard12@gmail.com>
2022-11-18 16:11:32 +0800

    add new birthday

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const affirmations = [
-  `I wrote in my notes: "she put off work a little to go get me a scone because i never wake up early enough but i love scones and i love her". I love how thoughtful you are.`,
-  ``,
+  `In my notes: "she put off work a little to go get me a scone because i never wake up early enough but i love scones and i love her". I love how thoughtful you are as a person.`,
+  `In my notes: "the way that sarah likes me laughing, or likes me making dumb blee bloo blop blop noises, makes me love her". You are the most accepting partner I've ever had.`,
+  `In my notes: "you know what it is, i think she's a really brave, that's what i mean by effort, it's that she feels fully the difficulty of what she faces but she tackles it nevertheless, every time." I love you.`,
+  `I wish you could see how my therapist smiled when I told her that because you know I struggle with people pleasing, you kept asking me to think about what I want to do. I'm grateful for how caring you are as a person.`,
+  `The time I went ziplining, I was so scared but I whispered "I love my girlfriend" and jumped. I just wanted to know that if I got into a freak accident that somebody there could still relay the message.`,
 ];
 
 const menuItems = [
   { href: "./index.html", label: "Show me the first letter." },
   { href: "./birthday-1.html", label: "It's my birthday today!" },
   { href: "./airplane.html", label: "Thoughts on an airplane." },
+  {
+    href: "./birthday-2.html",
+    label: "It's my birthday again!",
+  },
 ];
 
 function clickAffirmation() {
